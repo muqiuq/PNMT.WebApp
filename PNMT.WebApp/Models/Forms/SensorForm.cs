148327732d14d4bc73d5038dec8e98862f9375df
alptbz <info@uisa.ch>
2023-08-08 02:03:28 +0200

    create new sensor form

---
+++ PNMT.WebApp/Models/Forms/SensorForm.cs
@@ -0,0 +1,21 @@
+﻿using PNMTD.Lib.Models.Enum;
+
+namespace PNMT.WebApp.Models.Forms
+{
+    public class SensorForm
+    {
+        public Guid Id { get; set; }
+
+        public SensorType Type { get; set; }
+
+        public string Name { get; set; }
+
+        public string? TextId { get; set; }
+
+        public bool Enabled { get; set; }
+
+        public int Interval { get; set; }
+
+        public int GracePeriod { get; set; }
+    }
+}
