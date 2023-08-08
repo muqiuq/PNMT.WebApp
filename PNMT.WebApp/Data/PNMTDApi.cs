6807bd6b2ce51d6eeeae9fb5924207aaa51f8e3e
alptbz <info@uisa.ch>
2023-08-08 18:10:26 +0200

    implemented sensor, notificationrule, refactoring

--- PNMT.WebApp/Data/PNMTDApi.cs
+++ PNMT.WebApp/Data/PNMTDApi.cs
@@ -17,6 +17,7 @@ namespace PNMT.WebApp.Data
 
         public readonly HostApiCrud Hosts;
         public readonly SensorApiCrud Sensors;
+        public readonly NotificationRuleApiCrud NotificationRules;
 
         public PNMTDApi(HttpClient httpClient, JwtTokenProvider jwtTokenProvider)
         {
@@ -25,8 +26,17 @@ namespace PNMT.WebApp.Data
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwtTokenProvider.JwtToken);
             this.Hosts = new HostApiCrud(httpClient);
             this.Sensors = new SensorApiCrud(httpClient);
+            this.NotificationRules = new NotificationRuleApiCrud(httpClient);
         }
 
+        public string GetSensorEventUrl(SensorPoco sensor) {
+            return $"{BaseAddress}/event/{sensor.Id}/";
+        }
+
+        public string GetSensorEventUrl(SensorPoco sensor, string code, string message)
+        {
+            return $"{GetSensorEventUrl(sensor)}{code}/{message}";
+        }
 
     }
 }
---
+++ PNMT.WebApp/Models/Forms/NotificationRuleForm.cs
@@ -0,0 +1,19 @@
+﻿using PNMTD.Lib.Models.Enum;
+using System.ComponentModel.DataAnnotations;
+
+namespace PNMT.WebApp.Models.Forms
+{
+    public class NotificationRuleForm
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Recipient { get; set; }
+
+        public bool Enabled { get; set; }
+
+        public NotificationRuleType Type { get; set; }
+
+    }
+}
