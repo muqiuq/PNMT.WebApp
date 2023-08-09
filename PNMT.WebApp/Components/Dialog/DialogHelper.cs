2dbc27f76d3fc26e555de24d5e587ac527507151
alptbz <info@uisa.ch>
2023-08-09 23:51:51 +0200

    continued design adaptation, added delete dialog

---
+++ PNMT.WebApp/Components/Dialog/DialogHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using PNMT.WebApp.Data;
+
+namespace PNMT.WebApp.Components.Dialog
+{
+  public static class DialogHelper
+  {
+
+    public static void ShowDeleteDialog(IDialogService dialogService, Func<Task> onAccept)
+    {
+      var parameters = new DialogParameters<OkCancelDialogComp>();
+      parameters.Add(x => x.ContentText, "Do you really want to delete this entity? This process cannot be undone.");
+      parameters.Add(x => x.ButtonText, "Delete");
+      parameters.Add(x => x.Color, Color.Error);
+      parameters.Add(x => x.OnAccept, onAccept);
+
+      var options = new DialogOptions() { CloseButton = true, MaxWidth = MaxWidth.ExtraSmall };
+
+      dialogService.Show<OkCancelDialogComp>("Delete", parameters, options);
+    }
+  }
+}
