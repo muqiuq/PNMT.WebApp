86e46c1e966bf3c3b43064c13c96e8d3f5154b9e
alptbz <info@uisa.ch>
2023-08-15 01:59:02 +0200

    Implemented MailInput Crud

---
+++ PNMT.WebApp/Models/Forms/MailInputForm.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PNMT.WebApp.Models.Forms
+{
+    public class MailInputForm
+    {
+        public Guid Id { get; set; }
+
+        public bool Enabled { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string Name { get; set; }
+
+        public string? ContentTest { get; set; }
+
+        public string? SenderTest { get; set; }
+
+        public int? OkCode { get; set; }
+
+        public string? OkTest { get; set; }
+
+        public int? FailCode { get; set; }
+
+        public string? FailTest { get; set; }
+
+        [Required]
+        public int DefaultCode { get; set; }
+
+        [Required]
+        public Guid? SensorOutputId { get; set; }
+    }
+}
