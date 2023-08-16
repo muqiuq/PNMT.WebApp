d6a4c92dd81d5b082aabf6b25eb27ed409af930e
alptbz <info@uisa.ch>
2023-08-16 13:31:09 +0200

    added keyvalue crud

---
+++ PNMT.WebApp/Models/Forms/KeyValueForm.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PNMT.WebApp.Models.Forms
+{
+    public class KeyValueForm
+    {
+        public Guid Id { get; set; }
+
+        public bool Enabled { get; set; } = true;
+
+        public string Note { get; set; } = "";
+
+        [Required(AllowEmptyStrings = false)]
+        public string Key { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string Value { get; set; }
+
+        public bool IsReadOnly { get; set; } = false;
+    }
+}
