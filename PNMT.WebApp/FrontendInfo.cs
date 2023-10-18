79081bf3601797754ceccd165b50c18a15089843
alptbz <info@uisa.ch>
2023-10-18 11:25:34 +0200

    added version

---
+++ PNMT.WebApp/FrontendInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+namespace PNMT.WebApp
+{
+    public class FrontendInfo
+    {
+
+        public string Version
+        {
+            get
+            {
+                return Assembly.GetEntryAssembly().GetName().Version.ToString();
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return Assembly.GetEntryAssembly().GetName().Name ?? "PNMT.WebApp";
+            }
+        }
+
+    }
+}
